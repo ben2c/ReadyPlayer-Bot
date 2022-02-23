ac0d369a695ada6e8905f3cfbfbbb06ce2f12c25
ben2c <bencheng200@gmail.com>
2022-02-23 12:59:44 -0600

    added nrall function and updated r up status message

--- index.js
+++ index.js
@@ -89,9 +89,9 @@ bot.on('message', message => {
                 playerArrString1.push(message.author.username);
                 teamSpaceAvailable1--;
                 if(teamSpaceAvailable1 >1)
-                    message.channel.send(`You\'ve been added to queue 1\nTeam: ${playerArrString1}, missing ${teamSpaceAvailable1} more players`)
+                    message.channel.send(`Queue 1: ${playerArrString1}, missing ${teamSpaceAvailable1} more players`)
                 if(teamSpaceAvailable1 === 1)
-                    message.channel.send(`You\'ve been added to queue 1\nTeam: ${playerArrString1}, missing ${teamSpaceAvailable1} more player!`)
+                    message.channel.send(`Queue 1: ${playerArrString1}, missing ${teamSpaceAvailable1} more player!`)
                 if(playerArray1.length === 5)
                     message.channel.send(`Get in here ${playerArray1}\n IT\'S TIME TO PLAY ${gameName1}!??!`);
                 break;
@@ -111,9 +111,9 @@ bot.on('message', message => {
                 playerArrString2.push(message.author.username);
                 teamSpaceAvailable2--;
                 if(teamSpaceAvailable2 >1)
-                    message.channel.send(`You\'ve been added to queue 2\nTeam: ${playerArrString2}, missing ${teamSpaceAvailable2} more players`)
+                    message.channel.send(`Queue 2: ${playerArrString2}, missing ${teamSpaceAvailable2} more players`)
                 if(teamSpaceAvailable2 === 1)
-                    message.channel.send(`You\'ve been added to queue 2\nTeam: ${playerArrString2}, missing ${teamSpaceAvailable2} more player!`)
+                    message.channel.send(`Queue 2: ${playerArrString2}, missing ${teamSpaceAvailable2} more player!`)
                 if(playerArray2.length === 5)
                     message.channel.send(`Get in here ${playerArray2}\n IT\'S TIME TO PLAY ${gameName2}!??!`);
                 break;
@@ -133,9 +133,9 @@ bot.on('message', message => {
                 playerArrString3.push(message.author.username);
                 teamSpaceAvailable3--;
                 if (teamSpaceAvailable3 > 1)
-                    message.channel.send(`You\'ve been added to queue 3\nTeam: ${playerArrString3}, missing ${teamSpaceAvailable3} more players`)
+                    message.channel.send(`Queue 3: ${playerArrString3}, missing ${teamSpaceAvailable3} more players`)
                 if (teamSpaceAvailable3 === 1)
-                    message.channel.send(`You\'ve been added to queue 3\nTeam: ${playerArrString3}, missing ${teamSpaceAvailable3} more player!`)
+                    message.channel.send(`Queue 3: ${playerArrString3}, missing ${teamSpaceAvailable3} more player!`)
                 if (playerArray3.length === 4)
                     message.channel.send(`Get in here ${playerArray3}\n IT\'S TIME TO PLAY ${gameName3}!??!`);
                 break;
@@ -231,7 +231,44 @@ bot.on('message', message => {
             else {
                 message.channel.send('You\'re already removed from queue 3');
                 break;
-            }       
+            }
+        case 'nr1':
+            if (playerArray1.indexOf('<@' + message.author.id + '>') !== -1) {
+                let x = playerArray1.indexOf('<@' + message.author.id + '>');
+                playerArray1.splice(x, 1)
+                playerArrString1.splice(x, 1)
+                teamSpaceAvailable1++;
+                message.channel.send('You\'ve been removed from queue 1')
+                break;
+            }
+            else {
+                message.channel.send('You\'re already removed from queue 1');
+                break;
+            }
+        case 'nrall':
+            message.channel.send('You\'ve been removed from all queues')
+            if (playerArray1.indexOf('<@' + message.author.id + '>') !== -1) {
+                let x = playerArray1.indexOf('<@' + message.author.id + '>');
+                playerArray1.splice(x, 1)
+                playerArrString1.splice(x, 1)
+                teamSpaceAvailable1++;
+            }
+
+            if (playerArray2.indexOf('<@' + message.author.id + '>') !== -1) {
+                let x = playerArray2.indexOf('<@' + message.author.id + '>');
+                playerArray2.splice(x, 1)
+                playerArrString2.splice(x, 1)
+                teamSpaceAvailable2++;
+            }
+
+
+            if (playerArray3.indexOf('<@' + message.author.id + '>') !== -1) {
+                let x = playerArray3.indexOf('<@' + message.author.id + '>');
+                playerArray3.splice(x, 1)
+                playerArrString3.splice(x, 1)
+                teamSpaceAvailable3++;
+            }
+        break;
         case 'clear1':
             playerArray1.splice(0, playerArray1.length);
             message.channel.send('Queue 1 cleared');
