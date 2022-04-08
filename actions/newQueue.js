12c6eebe34dd84429f3e2f6dd2e6489db5d8d047
ben2c <bencheng200@gmail.com>
2022-04-07 19:58:56 -0500

    added a check for existing queues

--- actions/newQueue.js
+++ actions/newQueue.js
@@ -1,7 +1,15 @@
 import { playerArr, playerArrString, gameNameArr, queueSize } from "../main.js";
 
 export const newQueue = (args, message) => {
-  if (args[1] && args[2] && args[2].match(/^\d+$/) && args[2] > 0) {
+
+  loweredArr = gameNameArr.map(e => {
+    return e.toLowerCase();
+  })
+
+  if (loweredArr.indexOf(args[1].toLowerCase()) !== -1) {
+    message.channel.send(`This game queue already exist`);
+  }
+  else if (args[1] && args[2] && args[2] > 0 && args[2].match(/^\d+$/)) {
     message.channel.send(`A new queue has been added for ${args[1]} and you have been added to the queue`);
     playerArr.push(['<@' + message.author.id + '>']);
     playerArrString.push([message.author.username]);
@@ -11,7 +19,6 @@ export const newQueue = (args, message) => {
   else {
     message.channel.send(`Please state a game + proper queue size`);
   }
-
   if (args[2] == 1) {
     message.channel.send(`Get in here ${playerArr[playerArr.length - 1]} It's time to play ${gameNameArr[playerArr.length - 1]}!... by yourself... sad...`);
   }
